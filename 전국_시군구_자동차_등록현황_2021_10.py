00204d351b4b07d07e685a414a0fbb20969f67b7
bcyoo <89197951+bcyoo@users.noreply.github.com>
2021-11-24 16:12:22 +0900

    Add files via upload

--- 전국_시군구_자동차_등록현황_2021_10.py
+++ 전국_시군구_자동차_등록현황_2021_10.py
@@ -18,6 +18,7 @@
 # 한글폰트 사용
 import platform
 from matplotlib import font_manager, rc
+import matplotlib.pyplot as plt
 plt.rcParams['axes.unicode_minus'] = False
 
 if platform.system() == 'Darwin':
@@ -45,102 +46,119 @@ import folium
 # %matplotlib inline 
 # -
 
-## geopy로 주소 간단하게 지오코딩 하는 법
+import networkx as nx 
 from geopy.geocoders import Nominatim
 import osmnx as ox 
-
-pd.set_option('display.max_columns', None)
-pd.set_option('display.max_rows', None)
-
-df = pd.read_csv('DATA/시군구_자동차_등록현황_2021_10.csv', engine='python')
-df
-
-df["시군구"] = df["시"].map(str) + " " + df["군구"]
-
-df = df.loc[df['군구'] != '계']
-df
-
-df = df1.loc[df['시'] != '합계' ]
-df
-
-df.columns.tolist()
-
-df = df[['시군구', '시', '군구', '자동차 등록 현황']]
-df
-
-df.to_excel(excel_writer='address.xlsx',encoding = 'cp949')
+import shapely 
+import pandas as pd
+from shapely import geometry
+from descartes.patch import PolygonPatch
+from shapely.geometry import LineString
 
 # +
-# key : E07005C0-0CEE-30AC-B9D7-84F23F520E05
-#-*-coding: utf-8 -*-
-import requests
-import openpyxl
-import json
+## 시군구 자동차 등록 현황 csv 파일 불러오기
+df_1 = pd.read_csv('DATA/시군구_자동차_등록현황_2021_10.csv', engine='python')
 
-## 엑셀 파일 오픈
-filename = "address.xlsx"
-exelFile = openpyxl.load_workbook(filename)
+## 시군구 col 만들고 시 col + 군구 col
+df_1["시군구"] = df_1["시"].map(str) + " " + df_1["군구"]
 
-## 시트 설정
-sheet = exelFile.worksheets[0]
+## 필요없는 계 row 제거
+df_1 = df_1.loc[df_1['군구'] != '계']
 
-## 데이터 가져오기
-rowCount = 1
-for row in sheet.rows:	
+## 필요없는 계 row 제거
+df_1 = df_1.loc[df_1['시'] != '합계' ]
 
-	try:
-		## geocoder 호출
-		r = requests.get("http://apis.vworld.kr/new2coord.do?q="+row[0].value+"&output=json&epsg=epsg:4326&apiKey=E07005C0-0CEE-30AC-B9D7-84F23F520E05")
-
-		## cell 설정 [ (10)J1 ~ J* : 위도 / (11)K1 ~ K* : 경도]
-		lat_cell = sheet.cell(row = rowCount, column = 2)
-		lng_cell = sheet.cell(row = rowCount, column = 3)
-
-		## 데이터 추가
-		lat_cell.value = r.json()["EPSG_4326_Y"]
-		lng_cell.value = r.json()["EPSG_4326_X"]
-	except KeyError as ke:
-		lat_cell.value = 0
-		lng_cell.value = 0
-	except TypeError as te:
-		lat_cell.value = 0
-		lng_cell.value = 0
-		
-	rowCount = rowCount + 1
-
-## 데이터 저장
-exelFile.save("address.xlsx")
-# -
-
-df_1 = pd.read_csv('./address.csv')
+## 컬럼명 재설정
+df_1 = df_1[['시군구', '시', '군구', '자동차 등록 현황']]
 df_1
 
-(ggplot(df_1)  ## gg plot 으로 시각화
+# -
+
+## 지오코딩에 필요한 리스트 시군구 생성
+address = df_1['시군구'].tolist()
+address
+
+# +
+import googlemaps
+import pandas as pd
+
+my_key = "AIzaSyCUKB2z6sahUkbf-MmWmpOf7ZM56Mte118"
+maps = googlemaps.Client(key=my_key)  # my key값 입력
+lat = []  #위도
+lng = []  #경도
+
+# 위치를 찾을 장소나 주소를 넣어준다.
+places = address
+
+i=0
+for place in places:   
+    i = i + 1
+    try:
+        print("%d번 인덱스에서 %s의 위치를 찾고있습니다"%(i, place))
+        geo_location = maps.geocode(place)[0].get('geometry')
+        lat.append(geo_location['location']['lat'])
+        lng.append(geo_location['location']['lng'])
+        
+
+    except:
+        lat.append('')
+        lng.append('')
+        print("%d번 인덱스 위치를 찾는데 실패했습니다."%(i))
+
+
+# 데이터프레임만들어 출력하기
+df = pd.DataFrame({'위도':lat, '경도':lng}, index=places)
+print(df)
+
+# +
+# pd.set_option('display.max_columns', None)
+# pd.set_option('display.max_rows', None)
+# -
+
+## 경도 위도 DataFrame 재설ㅈㅇ
+df.info()
+
+## index 재설정
+df = df.reset_index()
+
+df
+
+df.columns
+
+df = df.rename(columns={'index' : '시군구'})
+
+df_1
+
+df
+
+df_3 = pd.merge(df_1, df,how='outer',on='시군구')
+
+df_3
+
+df_3.loc[df_3.시군구 == '경남 창원 마산회원', ('위도', '경도')] = (35.22215731828623, 128.57918402242896)
+
+df_3
+
+
+
+(ggplot(df_3)  ## gg plot 으로 시각화
  + aes(x='경도', y='위도', color='자동차 등록 현황')
  + geom_point() # 점포인트로 찍음
  + theme(text=element_text(family=font_name))
 ) 
 
-df_1
+df_3.dtypes
 
-df
-
-df_1.drop(columns=['시군구', '자동차 등록 현황'], inplace=True)
-df_1
-
-df_1
-
-df_2 = df.merge(df_1, left_index=True, right_index=True)
-df_2.info()
-
-df_2.columns = ['주소', '시도', '구군', '자동차등록현황', '위도', '경도']
-df_2
+df_3 = df_3.astype({'위도':'float', '경도':'float'})
 
 # +
-df_2.shape
-map = folium.Map(location=[df_2['위도'].mean(), df_2['경도'].mean()], zoom_start=13)
+df_3.shape
+map = folium.Map(location=[df_3['위도'].mean(), df_3['경도'].mean()], zoom_start=13)
 
-for n in df_2.index:
-    park_name = df_2.loc[n, '자동차등록현황'] + ' - ' + df_2.loc[n, '주소']
-    folium.Marker([df_2.loc[n, '위도'], df_2.loc[n, '경도']], popup=park_name).add_to(map)
+for n in df_3.index:
+    park_name = df_3.loc[n, '자동차 등록 현황'] + ' - ' + df_3.loc[n, '시군구']
+    folium.Marker([df_3.loc[n, '위도'], df_3.loc[n, '경도']], popup=park_name).add_to(map)
 map
+# -
+
+
